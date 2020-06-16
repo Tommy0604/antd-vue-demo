eb9ff55d3115c8a3aacd018457a71eba8d31a699
Tommy0604 <904859162@qq.com>
2020-06-16 15:36:54 +0800

    Add：nprogress.css

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import Home from "../views/Home.vue";
 import NotFound from "../views/Exception/404";
 
